3188b84350371bd34f9cbf01e3f6bc1c48d1d35f
ravi31186 <ravi31186@gmail.com>
2025-09-20 17:19:46 -0400

    removed roter from main.jsx

--- portfolio-site/src/main.jsx
+++ portfolio-site/src/main.jsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
